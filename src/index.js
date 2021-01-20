3fec203ba371bbc8b7f4c62c8fc65d2d13995649
Shouvik Bajpayee <bajpayeeshouvik@gmail.com>
2021-01-20 16:54:07 +0530

    solved CORS problem

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./styles.css";
 import Routes from "./Routes";
+import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(<Routes />, document.getElementById("root"));
 
